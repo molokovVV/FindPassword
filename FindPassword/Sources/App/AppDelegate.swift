380caedaedba61e17037ea1ae4dcf3a96bb9eba7
Vitaliy <vmolokov59@gmail.com>
2023-03-16 20:17:25 +0300

    commit

---
+++ FindPassword/Sources/App/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  FindPassword
+//
+//  Created by Виталик Молоков on 12.03.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, 
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
